4a94a3133c4e34ba7331e57556d646f95119571f
AhmadIbrahim99 <43531161+AhmadIbrahim99@users.noreply.github.com>
2022-05-15 23:54:42 +0300

    feeds screen

--- lib/inner_screens/on_sale_screen.dart
+++ lib/inner_screens/on_sale_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:grocery_app/services/utils.dart';
+import 'package:grocery_app/widgets/on_sale_widget.dart';
 import 'package:grocery_app/widgets/text_widget.dart';
 
 class OnSaleScreen extends StatelessWidget {
@@ -11,6 +12,7 @@ class OnSaleScreen extends StatelessWidget {
   const OnSaleScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    bool isEmpty = false;
     Color color = Utils(context).getColor;
     Size size = Utils(context).getScreenSize;
     return Scaffold(
@@ -32,12 +34,36 @@ class OnSaleScreen extends StatelessWidget {
           isTilte: true,
         ),
       ),
-      body: GridView.count(
-        padding: EdgeInsets.zero,
-        crossAxisCount: 2,
-        childAspectRatio: size.width / (size.height * 0.45),
-        children: List.generate(16, (index) => const OnSaleScreen()),
-      ),
+      body: isEmpty
+          ? Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/images/box.png",
+                      scale: 1.5,
+                    ),
+                    Text(
+                      "No products on sale yet!, \n Stay tuned",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: color,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : GridView.count(
+              padding: EdgeInsets.zero,
+              crossAxisCount: 2,
+              childAspectRatio: size.width / (size.height * 0.45),
+              children: List.generate(16, (index) => const OnSaleWidget()),
+            ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:grocery_app/inner_screens/feeds_screen.dart';
 import 'package:grocery_app/inner_screens/on_sale_screen.dart';
 import 'package:grocery_app/provider/dark_theme_provider.dart';
 import 'package:grocery_app/services/global_methods.dart';
@@ -120,7 +121,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   // const Spacer(),
                   TextButton(
-                      onPressed: () {},
+                      onPressed: () => GlobalMethods.navigateTo(
+                          ctx: context, name: FeedsScreen.routeName),
                       child: TextWidget(
                           maxLines: 1,
                           text: "Browse all",
