f15a5e26dbd9d309ba95debe67a0830c6033774a
AhmadIbrahim99 <43531161+AhmadIbrahim99@users.noreply.github.com>
2022-10-03 01:32:44 +0300

    testing keys

---
+++ lib/consts/mykeys.dart
@@ -0,0 +1,9 @@
+import 'dart:collection';
+
+mixin MyKeys {
+  static const String NAMEREG = "NAMEREG";
+  static const String PASSWORDREG = "PASSWORDREG";
+  static const String EMAILREG = "EMAILREG";
+  static const String ADDRESSREG = "ADDRESSREG";
+  static const String SIGNUP_BUTTON = "SIGNUP_BUTTON";
+}
